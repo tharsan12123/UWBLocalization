ef8bb30a168cf47e86e6a3a902d08e8a228a3660
tharsan12123 <tharsant884@gmail.com>
2025-07-27 15:25:58 +0530

    tharsan

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/UWBLocalization/', // 👈 Important for GitHub Pages
+  base: '/UWBLocalization/', // Your repo name
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
